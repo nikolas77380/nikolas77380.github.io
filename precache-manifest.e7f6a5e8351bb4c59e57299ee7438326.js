252db67a2be2561aa6c2871fa24ce94ac4e47fa4
Mykola.kipniak <mykola.kipniak@globallogic.com>
2019-08-22 16:11:05 +0300

    Updates

---
+++ precache-manifest.e7f6a5e8351bb4c59e57299ee7438326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "430ac6da0677c3e7025ba0d98eded135",
+    "url": "/tefl-crm/static/media/brand.430ac6da.svg"
+  },
+  {
+    "revision": "eb7706551c9002751939",
+    "url": "/tefl-crm/static/js/runtime~main.52e93b01.js"
+  },
+  {
+    "revision": "8f03b91bcae4def304bb",
+    "url": "/tefl-crm/static/js/main.f7b2bf01.chunk.js"
+  },
+  {
+    "revision": "34de0839b6ccf220464c",
+    "url": "/tefl-crm/static/js/2.834fb316.chunk.js"
+  },
+  {
+    "revision": "8f03b91bcae4def304bb",
+    "url": "/tefl-crm/static/css/main.b3c5ac1c.chunk.css"
+  },
+  {
+    "revision": "081da816c13f6ee429eea247fc96c5e5",
+    "url": "/tefl-crm/index.html"
+  }
+];
